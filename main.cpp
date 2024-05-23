f6575e5f747efd50903dd0bdb9ca50c2abc0184e
jorge1208 <149629001+jorge1208@users.noreply.github.com>
2024-05-23 10:21:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,40 +1,9 @@
-#include <iostream>
-#include <map>
-#include <string>
+#include "funciones.h"
 using namespace std;
-
-class SistemaLogin {
-private:
-    map<string, string> usuarios;
-
-public:
-    SistemaLogin() {
- 
-        usuarios["juan"] = "123";
-        usuarios["maria"] = "456";
-    }
-
-    bool iniciarSesion(const string& nombreUsuario, const string& contrasena) {
-        return usuarios[nombreUsuario] == contrasena;
-    }
-};
 
 int main() {
     SistemaLogin sistema;
-
-    string nombreUsuario, contrasena;
-    cout << "Bienvenido al sistema de inicio de sesion" << endl;
-    cout << "Ingrese su nombre de usuario: ";
-    cin >> nombreUsuario;
-
-    cout << "Ingrese su contrasena: ";
-    cin >> contrasena;
-
-    if (sistema.iniciarSesion(nombreUsuario, contrasena)) {
-        cout << "Inicio de sesion exitoso" << endl;
-    } else {
-        cout << "Usuario o contrasena incorrectos." << endl;
-    }
+    sistema.inicio();
 
     return 0;
 }
