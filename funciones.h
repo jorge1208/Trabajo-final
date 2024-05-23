12bfe8c7daff3c3637bca0c121fc2352d9a4e41b
jorge1208 <149629001+jorge1208@users.noreply.github.com>
2024-05-23 10:56:58 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -6,38 +6,59 @@
 using namespace std;
 
 
-class menu
+class plantillas
 {
     public: 
     char hotel[24] = " E  L    C  I  E  L  O ";
     void imprimir_encabezado();
+	void imprimir_menu();
 };
-void menu::imprimir_encabezado()
+void plantillas::imprimir_encabezado()
 {
     system("cls"); 
     cout<<"\x1b[48;2;91;10;45;38;2;255;255;255m-----------------------------------------------------------------------------------------------------\n";
     cout<<"                                       "<<hotel<<"                                       \n";
     cout<<"-----------------------------------------------------------------------------------------------------\x1b[m\n";
 }
+void plantillas :: imprimir_menu()
+{
+	imprimir_encabezado();
+	cout << "\x1b[48;2;173;173;173;38;2;0;0;0m   Escoja una opcion: "<< setw(79) << " \n";
+	cout << "   1. Habitaciones"<< setw(83) << " \n"<<"   2. Reservas"<< setw(87) << " \n"<<"   3. Contacto"<< setw(87) << " \n"<<"   4. Servicios"<< setw(88) << "   \x1b[m" << endl;
+	
+} 
+
+
+
+
+
+
+
+//Login empleado
 
 class SistemaLogin {
 private:
     map<string, string> usuarios;
 
 public:
-    SistemaLogin() {
- 
-        usuarios["juan"] = "123";
-        usuarios["maria"] = "456";
-    }
-
-    bool iniciarSesion(const string& nombreUsuario, const string& contrasena) {
-        return usuarios[nombreUsuario] == contrasena;
-    }
-    void inicio()
-    {
-        string nombreUsuario, contrasena;
-        menu _menu1;
+    SistemaLogin();
+	bool llave1 {false};
+    bool iniciarSesion(const string& nombreUsuario, const string& contrasena);
+    void inicio();
+};
+SistemaLogin::SistemaLogin()
+{ 
+    usuarios["juan"] = "123";
+    usuarios["maria"] = "456";
+}
+bool SistemaLogin::iniciarSesion(const string& nombreUsuario, const string& contrasena)
+{
+	return usuarios[nombreUsuario] == contrasena;
+}
+void SistemaLogin::inicio()
+{
+	string nombreUsuario, contrasena;
+        plantillas _menu1;
         _menu1.imprimir_encabezado();
         cout << "\x1b[48;2;173;173;173;38;2;0;0;0m|  Bienvenido al sistema de inicio de sesion" << setw(59)<< " \x1b[m" << endl;
         cout << "  Ingrese su nombre de usuario: " << setw(73)<< " \n"<< setw(15)<<"  ";
@@ -48,10 +69,65 @@ public:
 
         if (iniciarSesion(nombreUsuario, contrasena)) {
             cout << "Inicio de sesion exitoso" << endl;
+			llave1 = true;
         } else {
             cout << "Usuario o contrasena incorrectos." << endl;
         }
+    
+}
 
-    } 
+class  Cliente
+	{
+		
+		public:
+		
+		char nombre[128];
+		char dni[8];
+		char sexo[1];
+		char celular[9];
+		char domicilio[128];
+		int dia;
+		int mes;
+		int año;
+		
+		
+		Cliente(){
+			
+			
+			for (int i=0; i<128; i++)
+			{
+				nombre[i]='-';
+			}
+			
+			for (int i=0; i<8; i++)
+			{
+				dni[i]='-';
+			}
+			
+			for (int i=0; i<1; i++)
+			{
+				sexo[i]='-';
+			}
+			
+			for (int i=0; i<9; i++)
+			{
+				celular[i]='-';
+			}
+			
+			for (int i=0; i<128; i++)
+			{
+				domicilio[i]='-';
+			}
+			
+			dia=-1;
+			mes=-1;
+			año=-1;
+			
+		}
+	};
 
-};
+
+
+
+
+
