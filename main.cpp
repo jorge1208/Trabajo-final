47f027725853fbcd449f1ddcc954c44a3dd56572
FabianBustamante <63063558+FabianBustamante@users.noreply.github.com>
2024-05-23 00:08:29 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
-#include <iostrean>
-int main()
-{
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
 
-e
-hola
+class SistemaLogin {
+private:
+    map<string, string> usuarios;
 
-  return 0;
+public:
+    SistemaLogin() {
+ 
+        usuarios["juan"] = "123";
+        usuarios["maria"] = "456";
+    }
+
+    bool iniciarSesion(const string& nombreUsuario, const string& contrasena) {
+        return usuarios[nombreUsuario] == contrasena;
+    }
+};
+
+int main() {
+    SistemaLogin sistema;
+
+    string nombreUsuario, contrasena;
+    cout << "Bienvenido al sistema de inicio de sesion" << endl;
+    cout << "Ingrese su nombre de usuario: ";
+    cin >> nombreUsuario;
+
+    cout << "Ingrese su contrasena: ";
+    cin >> contrasena;
+
+    if (sistema.iniciarSesion(nombreUsuario, contrasena)) {
+        cout << "Inicio de sesion exitoso" << endl;
+    } else {
+        cout << "Usuario o contrasena incorrectos." << endl;
+    }
+
+    return 0;
 }
