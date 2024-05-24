e2532eb5a941fa465586f928d16cdf17e2d3e46f
jorge1208 <149629001+jorge1208@users.noreply.github.com>
2024-05-24 06:33:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 
 int main() {
-	plantillas principal;
-    SistemaLogin sistema;
-    sistema.inicio();
-	if (sistema.llave1)
-	{
-		principal.imprimir_menu();
-	}
+	
     return 0;
 }
