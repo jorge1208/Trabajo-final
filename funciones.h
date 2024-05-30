0ac2827642da5f4b2302128761d476df46468c16
jorge1208 <149629001+jorge1208@users.noreply.github.com>
2024-05-30 05:08:14 -0500

    Update clases.h

--- funciones.h
+++ funciones.h
@@ -5,7 +5,6 @@
 #include <iomanip>
 using namespace std;
 
-
 class Usuario {
 private:
     string nombre_;
@@ -18,247 +17,94 @@ private:
     int año_;
 
 public:
- 
-    Usuario() : dia_(0), mes_(0), año_(0) {}
+    Usuario();
+    Usuario(const string& nombre, const string& dni, const string& sexo, const string& celular, const string& domicilio, int dia, int mes, int año);
 
-  
-    Usuario(const string& nombre, const string& dni, const string& sexo, const string& celular, const string& domicilio, int dia, int mes, int año)
-        : nombre_(nombre), dni_(dni), sexo_(sexo), celular_(celular), domicilio_(domicilio), dia_(dia), mes_(mes), año_(año) {}
+    void setNombre(const string& nombre);
+    void setDni(const string& dni);
+    void setSexo(const string& sexo);
+    void setCelular(const string& celular);
+    void setDomicilio(const string& domicilio);
+    void setFechaNacimiento(int dia, int mes, int año);
 
-  
-    void setNombre(const string& nombre) {
-        nombre_ = nombre;
-    }
+    string getNombre() const ;
+    string getDni() const ;
+    string getSexo() const ;
+    string getCelular() const ;
+    string getDomicilio() const ;
+    int getDia() const ;
+    int getMes() const ;
+    int getAño() const ;
 
-    void setDni(const string& dni) {
-        dni_ = dni;
-    }
-
-    void setSexo(const string& sexo) {
-        sexo_ = sexo;
-    }
-
-    void setCelular(const string& celular) {
-        celular_ = celular;
-    }
-
-    void setDomicilio(const string& domicilio) {
-        domicilio_ = domicilio;
-    }
-
-    void setFechaNacimiento(int dia, int mes, int año) {
-        dia_ = dia;
-        mes_ = mes;
-        año_ = año;
-    }
-
-   
-    string getNombre() const {
-        return nombre_;
-    }
-
-    string getDni() const {
-        return dni_;
-    }
-
-    string getSexo() const {
-        return sexo_;
-    }
-
-    string getCelular() const {
-        return celular_;
-    }
-
-    string getDomicilio() const {
-        return domicilio_;
-    }
-
-    int getDia() const {
-        return dia_;
-    }
-
-    int getMes() const {
-        return mes_;
-    }
-
-    int getAño() const {
-        return año_;
-    }
-
-
-
-    void mostrarInformacion() const {
-        cout << "Nombre: " << nombre_ << endl;
-        cout << "DNI: " << dni_ << endl;
-        cout << "Sexo: " << sexo_ << endl;
-        cout << "Celular: " << celular_ << endl;
-        cout << "Domicilio: " << domicilio_ << endl;
-        cout << "Fecha de Nacimiento: " << dia_ << "/" << mes_ << "/" << año_ << endl;
-    }
+    void mostrarInformacion() const;
 };
 
 class Habitacion {
-	private:
-		int numero;
-		string tipo;       // Tipo de habitación: sencilla, doble, suite, etc.
-		bool disponible;
-		double precioPorNoche;
+private:
+    int numero;
+    string tipo;
+    bool disponible;
+    double precioPorNoche;
 
-	public:
-		// Constructor
-        Habitacion() : numero(-1), disponible(0), precioPorNoche(-1){}
-		Habitacion(int num, string tipoHabitacion, bool disp, double precio)
-			: numero(num), tipo(tipoHabitacion), disponible(disp), precioPorNoche(precio) {}
+public:
+    Habitacion() : numero(-1), disponible(0), precioPorNoche(-1) {}
+    Habitacion(int num, string tipoHabitacion, bool disp, double precio)
+        : numero(num), tipo(tipoHabitacion), disponible(disp), precioPorNoche(precio) {}
 
-		// Getters
-		int getNumero() const {
-			return numero;
-		}
+    int getNumero() const { return numero; }
+    string getTipo() const { return tipo; }
+    bool isDisponible() const { return disponible; }
+    double getPrecioPorNoche() const { return precioPorNoche; }
 
-		string getTipo() const {
-			return tipo;
-		}
+    void setNumero(int num) { numero = num; }
+    void setTipo(const string& tipoHabitacion) { tipo = tipoHabitacion; }
+    void setDisponible(bool disp) { disponible = disp; }
+    void setPrecioPorNoche(double precio) { precioPorNoche = precio; }
 
-		bool isDisponible() const {
-			return disponible;
-		}
-
-		double getPrecioPorNoche() const {
-			return precioPorNoche;
-		}
-
-		// Setters
-		void setNumero(int num) {
-			numero = num;
-		}
-
-		void setTipo(const string& tipoHabitacion) {
-			tipo = tipoHabitacion;
-		}
-
-		void setDisponible(bool disp) {
-			disponible = disp;
-		}
-
-		void setPrecioPorNoche(double precio) {
-			precioPorNoche = precio;
-		}
-
-		// Método para mostrar información de la habitación
-		void mostrarInformacion() const {
-			cout << "Numero: " << numero << endl;
-			cout << "Tipo: " << tipo << endl;
-			cout << "Disponible: " << (disponible ? "Si" : "No") << endl;
-			cout << "Precio por Noche: $" << precioPorNoche << endl;
-		}
+    void mostrarInformacion() const {
+        cout << "Numero: " << numero << endl;
+        cout << "Tipo: " << tipo << endl;
+        cout << "Disponible: " << (disponible ? "Si" : "No") << endl;
+        cout << "Precio por Noche: $" << precioPorNoche << endl;
+    }
 };
-class plantillas
-{
-    public: 
+
+class Plantillas {
+public:
     char hotel[24] = " E  L    C  I  E  L  O ";
-    void imprimir_encabezado();
-	void imprimir_menu();
+    void imprimirEncabezado();
+    void imprimirMenu();
 };
-void plantillas::imprimir_encabezado()
-{
-    system("cls"); 
-    cout<<"\x1b[48;2;91;10;45;38;2;255;255;255m-----------------------------------------------------------------------------------------------------\n";
-    cout<<"                                       "<<hotel<<"                                       \n";
-    cout<<"-----------------------------------------------------------------------------------------------------\x1b[m\n";
-}
-void plantillas :: imprimir_menu()
-{
-	imprimir_encabezado();
-	cout << "\x1b[48;2;173;173;173;38;2;0;0;0m   Escoja una opcion: "<< setw(79) << " \n";
-	cout << "   1. Habitaciones"<< setw(83) << " \n"<<"   2. Reservas"<< setw(87) << " \n"<<"   3. Contacto"<< setw(87) << " \n"<<"   4. Servicios"<< setw(88) << "   \x1b[m" << endl;
-	
-} 
 
-
-
-
-
-
-
-//Login empleado
 
 class SistemaLogin {
 private:
-    map<string, string> usuarios;
+    string* nombresUsuarios;
+    string* contrasenas;
+    int numUsuarios;
 
 public:
+    bool llave1 { false };
     SistemaLogin();
-	bool llave1 {false};
+    ~SistemaLogin();
     bool iniciarSesion(const string& nombreUsuario, const string& contrasena);
+    void agregarUsuario(const string& nombreUsuario, const string& contrasena);
     void inicio();
 };
-SistemaLogin::SistemaLogin()
-{ 
-    usuarios["juan"] = "123";
-    usuarios["maria"] = "456";
-}
-bool SistemaLogin::iniciarSesion(const string& nombreUsuario, const string& contrasena)
-{
-	return usuarios[nombreUsuario] == contrasena;
-}
-void SistemaLogin::inicio()
-{
-	string nombreUsuario, contrasena;
-        plantillas _menu1;
-        _menu1.imprimir_encabezado();
-        cout << "\x1b[48;2;173;173;173;38;2;0;0;0m|  Bienvenido al sistema de inicio de sesion" << setw(59)<< " \x1b[m" << endl;
-        cout << "  Ingrese su nombre de usuario: " << setw(73)<< " \n"<< setw(15)<<"  ";
-        cin >> nombreUsuario;
-
-        cout << "Ingrese su contrasena: ";
-        cin >> contrasena;
-
-        if (iniciarSesion(nombreUsuario, contrasena)) {
-            cout << "Inicio de sesion exitoso" << endl;
-			llave1 = true;
-        } else {
-            cout << "Usuario o contrasena incorrectos." << endl;
-        }
-    
-}
 
 class Hotel {
 private:
     string nombre;
     string ubicacion;
     string contacto;
-    Usuario usuarios[3];
-    Habitacion habitaciones[10];
+    int numeroUsuarios;
+    int numeroHabitaciones;
+    Usuario* usuarios;
+    Habitacion* habitaciones;
 
 public:
-    Hotel(const string& _nombre, const string& _ubicacion, const string& _contacto, Usuario _usuarios[], Habitacion _habitaciones[]);
-
+    Hotel(const string& _nombre, const string& _ubicacion, const string& _contacto, Usuario _usuarios[], int _numUsuarios, Habitacion _habitaciones[], int _numHabitaciones);
+    ~Hotel();
     void mostrarInformacion();
 };
 
-Hotel::Hotel(const string& _nombre, const string& _ubicacion, const string& _contacto, Usuario _usuarios[], Habitacion _habitaciones[])
-    : nombre(_nombre), ubicacion(_ubicacion), contacto(_contacto) 
-{
-    for (int i = 0; i < 3; ++i) {
-        usuarios[i] = _usuarios[i];
-    }
-    for (int i = 0; i < 10; ++i) {
-        habitaciones[i] = _habitaciones[i];
-    }
-}
-
-void Hotel::mostrarInformacion() {
-    cout << "Nombre del hotel: " << nombre << endl;
-    cout << "Ubicacion: " << ubicacion << endl;
-    cout << "Contacto: " << contacto << endl;
-    cout << "\nUsuarios:" << endl;
-    for (int i = 0; i < 3; ++i) {
-        usuarios[i].mostrarInformacion();
-        cout << "---------------------------" << endl;
-    }
-    cout << "\nHabitaciones:" << endl;
-    for (int i = 0; i < 10; ++i) {
-        habitaciones[i].mostrarInformacion();
-        cout << "---------------------------" << endl;
-    }
-}
