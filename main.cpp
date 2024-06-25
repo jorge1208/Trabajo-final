b8883c8e9a1803fc28562b61618548276b0facd5
IgnacioSkarakhod <143093444+IgnacioSkarakhod@users.noreply.github.com>
2024-06-25 07:54:10 -0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,33 +4,50 @@
 
 int main() {
     int opcion;
-    std::cout << "1.Opcion habitacion" << std::endl;
-    std::cout << "2.Opcion reserva" << std::endl;
-    std::cout << "3.Opcion cliente" << std::endl;
-    std::cout << "Elija una opcion: " << std::endl;
+    cout << "1.Opcion Empleado\n";
+    cout << "2.Opcion Cliente\n";
+    cout << "3.Opcion Habitacion\n";
+    cout << "Elija una opcion: ";
 
-    std::cin >> opcion;
+    cin >> opcion;
 
     switch(opcion){
         case 1: {
             int opcion2;
-            std::cout << "Opcion habitacion seleccionada" << std::endl;
-            std::cin >> opcion2;
+            cout << "Opcion empleado seleccionada";
+            cout << "1.Mostrar informacion\n";
+            cout << "2.Agregar empleado\n";
+            cout << "3.Buscar empleado\n";
+            cout << "Ingrese una opcion: ";
+            
+            cin >> opcion2;
+            
             switch(opcion2){
+                case 1:{
+                    cout << "Opcion mostrar informacion seleccionada";
+                }
+                case 2:{
+                    cout << "Opcion agregar empleado seleccionada";
+                }
+                case 3:{
+                    cout << "Opcion buscar empleado seleccionada";
+                }
+                default:
+                    cout << "Opcion invalida";
             } 
         }
         case 2: {
             int opcion3;
-            std::cout << "Opcion reserva seleccionada" << std::endl;
-            std::cin >> opcion3;
+            cout << "Opcion cliente seleccionada";
+            cin >> opcion3;
             switch(opcion3){
                 
             }
         }
         case 3: {
             int opcion4;
-            std::cout << "Opcion reserva seleccionada" << std::endl;
-            std::cin >> opcion4;
+            cout << "Opcion habitacion seleccionada" << std::endl;
+            cin >> opcion4;
             switch(opcion4){
                 
             }
